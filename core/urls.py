ac0b3548eed513bae1c11a6caf3f634771d43ba6
Rasha <r.alamleh@gmail.com>
2020-12-13 16:46:54 +0200

    login || logout ||signUp

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import (
-    remove_from_cart,
-    add_to_cart,
-    ProductView,
-    HomeView
-)
+from .views import *
+from django.contrib import admin
+from . import views
+
 
 app_name = 'core'
 
@@ -12,6 +10,9 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<pk>/', ProductView.as_view(), name='product'),
     path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart'),
+    path('',views.index,name="home"),
+    path('accounts/sign_up/',views.sign_up,name="sign-up")
+
 ]
 
