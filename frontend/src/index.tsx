686deafddb60593800ca3b52341360313e748694
imadaqel <emadaqel92@gmail.com>
2020-12-16 19:09:41 +0200

    productitem

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './redux/store';
+import "bulma/css/bulma.css";
 
 
 ReactDOM.render(
